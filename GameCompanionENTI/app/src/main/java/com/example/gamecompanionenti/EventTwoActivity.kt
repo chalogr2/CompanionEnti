e8a64e37eae717ee9145144d5e64997ee79a7981
Fernando <fernando.gonzalez@enti.cat>
2020-01-10 06:38:32 +0100

    Build for presentation

---
+++ GameCompanionENTI/app/src/main/java/com/example/gamecompanionenti/EventTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamecompanionenti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventTwoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_two)
+    }
+}
